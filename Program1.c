58327a76f87beb7552ba1c012cbcf2ab3bae6cbd
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-07-20 05:50:21 -0700

    Program to print hello world.

---
+++ Program1.c
@@ -0,0 +1,29 @@
+
+//////////////////////////////////////////////
+//
+//  Function Name : Main()
+//  Input : --
+//  Output : Hello world 
+//  Description : Program to print hello world.
+//  Date : 10/06/2021
+//  Author : Hrutik Takke
+//
+/////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh\n");
+    
+    return 0;
+}
+
+// ./Myexe -> OS -> main()  -> end of main -> return 0 to OS (it indicates success)
+
+// return from main to opertaing system
+// return 0     successful termitnation
+// return 1     abortive termination
+// return -1    erronious termination

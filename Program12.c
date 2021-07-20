b6801f4bda89e481b6b314421e71162b0df1c78e
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-07-20 08:27:10 -0700

    Accept number from user and return its factorial.

---
+++ Program12.c
@@ -0,0 +1,43 @@
+// Q. Accept number from user and return its factorial
+// Input : 5
+// Output : 5*4*3*2*1       (120)
+
+// Input :4
+// Output : 4*3*2*1  or 1*2*3*4     (24)
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : Fact
+//  Input : Integer.
+//  Output : Integer.
+//  Description : Accept number from user and return its factorial.
+//  Date : 20/07/2021
+//  Author : Hrutik Takke
+//
+////////////////////////////////////////////////////////////////////////////
+
+# include<stdio.h>
+int Fact(int iNo)
+{
+    int iCnt = 0, iMult = 1;
+    if(iNo < 0)
+    {  iNo = -iNo;  }
+   //       1          2          3
+    for(iCnt = 1 ; iCnt <= iNo; iCnt++)
+    {
+        iMult = iMult * iCnt;   // 4
+    }
+    return iMult;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    iRet = Fact(iValue);
+    printf("Result is : %d\n",iRet);
+    return 0;
+}

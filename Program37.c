4ab2ff0ac4f133b08e6a2eea4b24deb797111135
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-07-23 03:55:24 -0700

    Accept number from user and return the Frequency of 2.

---
+++ Program37.c
@@ -0,0 +1,43 @@
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : Frequency
+//  Input : Integer
+//  Output : Integer
+//  Description :  Accept number from user and return the Frequency of 2.
+//  Date : 23/07/2021
+//  Author : Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int Frequency(int iNo)
+{
+    int iCnt =0,iDigit = 0;
+    if(iNo < 0)
+    {iNo = -iNo;}
+    
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit == 2)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue1 = 0, iRet = 0;
+    printf("Enter number\n");
+    scanf("%d",&iValue1);
+    iRet = Frequency(iValue1);
+    
+    printf("Frequency of 2 is : %d\n",iRet);
+    
+    return 0;
+}

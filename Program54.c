55f081f944f04029b29be0d0a09ab348135f8b25
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-07-25 10:57:23 -0700

    Accept two numbers as( Rows and Column) from user and display the below pattern.

---
+++ Program54.c
@@ -0,0 +1,82 @@
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : pattern
+//  Input : Integer, Integer
+//  Output : void
+//  Description :  Accept two numbers as( Rows and Column) from user and display the below pattern.
+//  Date : 25/07/2021
+//  Author : Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+/*
+   Input : Row = 3 Column = 3
+   Output :
+            1   2   3
+            *   *   *
+            1   2   3
+ 
+ */
+
+#include<stdio.h>
+void Pattern(unsigned int iRow, unsigned int iCol)
+{
+    int i = 0, j = 0;
+    
+    for(i = 1; i<= iRow; i++)
+    {
+        for(j = 1; j<= iCol; j++)
+        {
+            if((i % 2) == 0)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("%d\t",j);
+            }
+        }
+        printf("\n");
+    }
+}
+ 
+int main()
+{
+    unsigned int iValue1 = 0, iValue2 = 0;
+    
+    printf("Enter number of rows\n");
+    scanf("%u",&iValue1);
+    printf("Enter number of columns\n");
+    scanf("%u",&iValue2);
+    
+    Pattern(iValue1,iValue2);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

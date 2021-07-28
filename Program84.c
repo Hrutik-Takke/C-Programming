9b7b3510982176c866fbbdfac6880f4f296aa144
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-07-28 08:44:54 -0700

    Accept the string from user and return the frequency of capital characters.

---
+++ Program84.c
@@ -0,0 +1,37 @@
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : CountCap
+//  Input : Charcter[]
+//  Output : integer
+//  Description :  Accept the string from user and return the frequency of capital characters.
+//  Date : 28/07/2021
+//  Author : Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+int CountCap(char str[])
+{
+    int iCnt =0;
+    if(str == NULL)
+    {        return 0; }
+    while(*str != '\0')
+    {
+        if((*str >= 'A') && (*str <= 'Z'))
+        {   iCnt++; }
+        str++;
+    }
+    return iCnt;
+}
+int main()
+{
+    char Arr[10];
+    int iRet = 0;
+    printf("Enter string\n");
+    scanf("%[^'\n']s",Arr);
+    
+    iRet = CountCap(Arr);   // Display(100)
+    printf("Frequency of capitals is: %d\n",iRet);
+    return 0;
+}

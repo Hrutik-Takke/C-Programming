b274cea9ec8c763f6092f1762b181339f56c994b
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-31 08:47:49 -0700

    Display the power of number.

---
+++ Power.c
@@ -0,0 +1,23 @@
+////////////////////////////////////////////
+//
+//  Input : enter a base and power 2 3.
+//  Output : Answer is 8.
+//  Description : Display the power of number
+//  Date : 31/08/2021
+//  Author : Hritik Takke
+//
+////////////////////////////////////////////
+
+#include <stdio.h>
+int main()
+{
+   int i = 1, base, power, ans = 1;
+   printf("Enter a base and power ");
+   scanf("%d %d",&base,&power);
+   while(i <= power)
+   {
+	  ans = ans * b;
+	  i++;
+   }
+   printf("Answer is %d",ans);
+}

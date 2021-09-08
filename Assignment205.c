e7dd6437eb31f36c1a739974ad72f00ac9418241
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-09-08 08:45:57 -0700

    Accept N numbers from user and display summation of digits of each number.

---
+++ Assignment205.c
@@ -0,0 +1,59 @@
+ 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DigitsSum
+//  Input : Integer[], Integer
+//  Output : Void
+//  Description : Accept N numbers from user and display summation of digits of each number.
+//  Date : 08/09/2021
+//  Author :  Hritik Takke
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ 
+Input : N : 6
+Elements : 8225 665 3 76 953 858
+Output : 17 17 3 13 17 21
+
+#include<stdio.h>
+#include<stdlib.h>
+void DigitsSum(int Arr[], int iLength)
+{
+   int iCnt = 0, iSum = 0;
+  if((Arr == NULL) || (iLength == 0))
+  {  
+  	 printf("Invalid Range");
+  	 return;
+  }
+  for(iCnt = 0; iCnt < iLength; iCnt++ ) 
+  {   
+  	 for( iSum = 0; Arr[iCnt] > 0; Arr[iCnt] = Arr[iCnt]/10)
+  	 {   
+  	 	iSum = iSum + Arr[iCnt]%10;
+  	 }
+  	  printf("%d\n",iSum);
+  }
+}
+int main()
+{
+  int iSize = 0,iRet = 0,iCnt = 0;
+  int *p = NULL;
+  printf("Enter number of elements");
+  scanf("%d",&iSize);
+  p = (int *)malloc(iSize * sizeof(int));
+  
+  if(p == NULL)
+  {
+	printf("Unable to allocate memory");
+	return -1;
+  }
+  printf("\nEnter %d elements ",iSize);
+  for(iCnt = 0; iCnt < iSize; iCnt++)
+  {
+	printf("\nEnter element : %d ",iCnt+1);
+	scanf("%d",&p[iCnt]);
+  }
+  DigitsSum(p, iSize);
+  free(p);
+  return 0;
+}

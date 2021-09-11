5d19ca3b100666d62170f4c389c8cc313a141811
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-09-11 10:11:11 -0700

    Write a program which accept string from user and count number of capital characters.

---
+++ Assignment231.c
@@ -0,0 +1,48 @@
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : CountCapital
+//  Input : char *
+//  Output : int
+//  Description : Write a program which accept string from user and count number of capital characters.
+//  Date : 11/09/2021
+//  Author :  Hritik Takke
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+
+#include<stdio.h>
+
+/*
+Input : “Marvellous Multi OS”
+Output : 4
+*/
+
+int CountCapital(char *str)
+{
+ int iCnt = 0;
+ if(str == NULL)
+ {
+ 	return -1;
+ }
+ while(*str != '\0')
+ {
+    if((*str >= 'A') && (*str <= 'Z'))
+    {
+    	iCnt++;
+    }
+    str++;
+ }
+ return iCnt;
+}
+int main()
+{
+  char arr[20];
+  int iRet = 0;
+  printf("Enter string ");
+  scanf("%[^'\n']s",arr);
+  iRet = CountCapital(arr);
+  printf("%d",iRet);
+  return 0;
+}  
+  

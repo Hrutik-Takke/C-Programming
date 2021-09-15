c83b60585dacd5f5f8538f8c61e58414b8672390
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-09-14 18:38:32 -0700

    Write a program which accept string from user and copy small characters of that string into another string.

---
+++ Assignment264.c
@@ -0,0 +1,40 @@
+ 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : StrCpySmall
+//  Input : char *,char *
+//  Output : void
+//  Description : Write a program which accept string from user and copy small characters of that string into another string.
+//  Date : 14/09/2021
+//  Author : Hritik Takke
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+/*
+Write a program which accept string from user and copy small characters of that string into another string.
+Input : “Marvellous multi OS”
+Output : “arvellous multi”
+*/
+
+void StrCpySmall(char *src, char *dest)
+{
+ // Fileter
+ while(*src != '\0')
+ {
+  if((*src >= 'a') && (*src != 'z') || (*src == ' '))
+  {
+    *dest = *src;
+     dest++;
+  }
+  src++;
+ }
+ *dest = '\0';
+}
+int main()
+{
+ char arr[30] = "Marvellous multi OS";
+ char brr[30]; // Empty string
+ StrCpySmall(arr,brr);
+ printf("%s",brr); // arvellous multi
+ return 0;
+}  
